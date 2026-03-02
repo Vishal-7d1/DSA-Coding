96547533325fd3ae05ee0c6d00a9f4c407037fe0
Vishal-7d1 <vyadav276203@gmail.com>
2026-03-02 13:08:01 +0530

    notes

---
+++ CPP/4_Function/4_Sum_of_digits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int sumOfDigits(int num){
+    int digSum=0;
+    while(num>0){
+        int lastDig=num%10;
+        num/=10;
+        digSum+=lastDig;
+    }
+    return digSum;
+}
+int main(){
+    cout<<"Sum of digits of 123 is: "<<sumOfDigits(123)<<endl;
+    return 0;
+}
