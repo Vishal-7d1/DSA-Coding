50bda6ce1a5ec4f79d7ac8117dbb3ecc5d25633d
Vishal-7d1 <vyadav276203@gmail.com>
2026-03-07 15:28:26 +0530

    n

---
+++ CPP/6_Pattern/3_A_Print.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Number: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        for(char j='A';j<='A'+i-1;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
