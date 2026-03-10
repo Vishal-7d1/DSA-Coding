386e314a56d2d24ff4c020500fd7e5dd3c90a03c
Vishal-7d1 <vyadav276203@gmail.com>
2026-03-10 19:09:51 +0530

    notes

--- CPP/6_Pattern/6_Number_Piramid.cpp
+++ CPP/6_Pattern/6_Number_Piramid.cpp
@@ -7,11 +7,12 @@ int main(){
     cin>> n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n-i;j++){
-            cout<<" "`;
+            cout<<" ";
         }
         for(int j=1;j<=i;j++){
             cout<<j;
         }
         cout<<endl;
+
     }
 }
