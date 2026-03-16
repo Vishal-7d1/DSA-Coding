064938a4411a401e7102f0727568c293e4c40b90
Vishal-7d1 <vyadav276203@gmail.com>
2026-03-17 00:19:20 +0530

    n

--- CPP/16_Array/1_Search_Element .cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int arr[6];
-    int index=-1,x;
-     
-}
---
+++ CPP/16_Array/search_element.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,x;
+    int arr[m];
+    cin>>m;
+    for(int i=0;i<=m;i++){
+        if(arr[i]==x)
+        return i;
+    }
+    return -1;
+   
+     
+}
