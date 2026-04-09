bd71f9a246cf2b227e29de4737b14e3cf12bd4f6
Vishal-7d1 <vyadav276203@gmail.com>
2026-04-09 23:59:01 +0530

    n

--- TOP20/1_Arrayinput.cpp
+++ TOP20/1_Arrayinput.cpp
@@ -1,10 +1,15 @@
-#include<iosteram>
+#include<iostream>
 using namespace std;
 int main(){
     int n;
     cin>>n;
-    int a[];
+    int a[n];
     for(int i=0;i<n;i++){
+
         cin>>a[i];
     }
+    for(int i=0;i<n;i++){
+        cout<<a[i];
+    }
+
 }
